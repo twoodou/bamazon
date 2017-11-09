5f3bef2c6eb64515e7b71b30b246a4629664c5eb
Thomas Wood <twoodou@outlook.com>
2017-11-08 19:12:09 -0600

    updated bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,24 +1,70 @@
 var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connection success!");
+  makeTable();
+});
 
 // Display all of the items available for sale.
-inquirer.prompt([
-
-]).then(function(input) {
-
-});
-
-
+var makeTable = function() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + "  ||  " + res[i].product_name + "  ||  " +
+        res[i].department_name + "  ||  " + res[i].price + "  ||  " + res[i].stock_quantity + "\n");
+    }
+    promptCustomer(res);
+  });
+};
 
 // Ask: Which product would you like to buy? (ID)
-inquirer.prompt([
+var promptCustomer = function(res) {
+  inquirer.prompt([{
+    type: "input",
+    name: "choice",
+    message: "Which item would you like to purchase?"
+  }]).then(function(answer) {
+    var correct = false;
+    for (var i = 0; i < res.length; i++) {
+      if (res[i].product_name == answer.choice) {
+        correct = true;
+        var product = answer.choice;
+        var id = i;
+        console.log(product);
 
-]).then(function(input) {
-
-});
-
-// Ask: How many units of the product would you like to buy?
-inquirer.prompt([
-
-]).then(function(input) {
-
-});
+        // Ask: How many units of the product would you like to buy?
+        inquirer.prompt({
+          type: "input",
+          name: "qty",
+          message: "How many would you like to buy?",
+          validate: function(value) {
+            if (isNaN(value) == false) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        }).then(function(answer) {
+          if ((res[id].stock_quantity - answer.qty) > 0) {
+            connection.query("UPDATE products SET stock_quantity='" + (res[id].stock_quantity - answer.qty) + "' WHERE product_name='" + product + "'", function(err, res2) {
+              console.log("Product Purchased!");
+              makeTable();
+            });
+          } else {
+            console.log("Not a valid selection!");
+            promptCustomer(res);
+          }
+        });
+      }
+    }
+  });
+};
